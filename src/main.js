e6dd20e63af5c73af72c494c74ac87ffa448b0c4
liwei <120247068@qq.com>
2021-06-09 22:29:50 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,38 +1,38 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'normalize.css';
-import axios from 'axios'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "normalize.css";
+import axios from "axios";
 //导入element
-import '@/plugins/element.js'
+import "@/plugins/element.js";
 //导入过滤器
-import '@/plugins/filter.js'
+import "@/plugins/filter.js";
 //导入全局样式
-import '@/assets/css/global.css'
+import "@/assets/css/global.css";
 //引入自定义图标
-import '@/assets/font_cloudMusic/iconfont.css'
+import "@/assets/font_cloudMusic/iconfont.css";
 //解决跨域
-import { VueJsonp } from 'vue-jsonp'
+import { VueJsonp } from "vue-jsonp";
 
-Vue.use(VueJsonp)
-Vue.prototype.$http = axios
-axios.defaults.baseURL = 'http://localhost:3000/'
-    // axios.defaults.baseURL = 'https://autumnfish.cn/'
-    // axios.defaults.baseURL = '/api'
-    // axios.defaults.baseURL = 'http://8.129.133.120:3000/'
+Vue.use(VueJsonp);
+Vue.prototype.$http = axios;
+axios.defaults.baseURL = "https://liweimusic.vercel.app/";
+// axios.defaults.baseURL = 'http://localhost:3000/'
+// axios.defaults.baseURL = 'https://autumnfish.cn/'
+// axios.defaults.baseURL = '/api'
+// axios.defaults.baseURL = 'http://8.129.133.120:3000/'
 axios.defaults.withCredentials = true;
 //引入vuescroll
 // import vuescroll from "vuescroll";
 //引入vuescroll样式
 // import "vuescroll/dist/vuescroll.css";
 
-
 // Vue.use(vuescroll); //使用
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    store,
-    render: h => h(App)
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
